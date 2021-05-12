44332a5f79a8e92f935e359df862073a12c640fe
arash <arashsameniy@gmail.com>
2021-05-12 21:16:54 +0430

    Namespace added to admin panel

--- sources/UI/admin_panel.cpp
+++ sources/UI/admin_panel.cpp
@@ -1,16 +1,22 @@
 #include "../../headers/UI/admin_panel.h"
 
-void banksList();
-void usersList();
-void addBank();
-void promoteUser();
-void demoteUser();
+namespace adminPanelNS
+{
+	void banksList();
+	void usersList();
+	void addBank();
+	void promoteUser();
+	void demoteUser();
+}
+
 
 void adminPanel(User authenticatedUser)
 {
+	using namespace adminPanelNS;
 	while (true)
 	{
 		system("cls");
+		std::cout << std::endl << bright << yellow << "             Welcome " << authenticatedUser.name << reset << std::endl;
 		std::cout << std::endl << cyan << "  Admin Panel :" << reset << std::endl << std::endl;
 		std::cout << "    1.Banks List" << std::endl;
 		std::cout << "    2.Users List" << std::endl;
@@ -45,153 +51,156 @@ void adminPanel(User authenticatedUser)
 	}
 }
 
-void banksList()
+namespace adminPanelNS
 {
-	system("cls");
-	std::cout << std::endl << cyan << "  Banks :" << reset << std::endl << std::endl;
-	std::vector<Bank> allBanks = Bank::getAllBanks();
-	for (int i = 0; i < allBanks.size(); i++)
+	void banksList()
 	{
-		std::cout << "    " << std::to_string(i + 1) << "." << std::endl;
-		std::cout << bright << cyan << "    ==========================================" << reset << std::endl;
-		std::cout << bright << yellow << "      BankId: " << allBanks[i].id << reset << std::endl << std::endl;
-		std::cout << "      Bank name: " << allBanks[i].name << std::endl << std::endl;
-		std::cout << "      Total Balance: " << allBanks[i].totalBalance << std::endl << std::endl;
-
-		std::cout << "      Managers: " << std::endl;
-		for (int j = 0; j < allBanks[i].managers.size(); j++)
-			std::cout << "        " << allBanks[i].managers[j] << std::endl;
-		std::cout << std::endl;
-
-		std::cout << "      Foundation Date: " << getDateTime(allBanks[i].foundationDate) << std::endl;
-		std::cout << bright << cyan << "    ==========================================" << reset << std::endl << std::endl;
-	}
-	std::string tmp;
-	std::getline(std::cin, tmp);
-}
-
-void usersList()
-{
-	system("cls");
-	std::cout << std::endl << cyan << "  Users :" << reset << std::endl << std::endl;
-	std::vector<User> allUsers = User::getAllUsers();
-	for (int i = 0; i < allUsers.size(); i++)
-	{
-		std::cout << "    " << std::to_string(i + 1) << "." << std::endl;
-		std::cout << bright << cyan << "    ==========================================" << reset << std::endl;
-
-		std::cout << "      Username: " << allUsers[i].username << std::endl;
-
-		std::string userType = "";
-		switch (allUsers[i].type)
+		system("cls");
+		std::cout << std::endl << cyan << "  Banks :" << reset << std::endl << std::endl;
+		std::vector<Bank> allBanks = Bank::getAllBanks();
+		for (int i = 0; i < allBanks.size(); i++)
 		{
-		case 1:
-			userType = "Normal user";
-			break;
-		case 2:
-			userType = "Bank manager";
-			break;
-		case 3:
-			userType = "Full admin";
-			break;
+			std::cout << "    " << std::to_string(i + 1) << "." << std::endl;
+			std::cout << bright << cyan << "    ==========================================" << reset << std::endl;
+			std::cout << bright << yellow << "      BankId: " << allBanks[i].id << reset << std::endl << std::endl;
+			std::cout << "      Bank name: " << allBanks[i].name << std::endl << std::endl;
+			std::cout << "      Total Balance: " << allBanks[i].totalBalance << std::endl << std::endl;
+
+			std::cout << "      Managers: " << std::endl;
+			for (int j = 0; j < allBanks[i].managers.size(); j++)
+				std::cout << "        " << allBanks[i].managers[j] << std::endl;
+			std::cout << std::endl;
+
+			std::cout << "      Foundation Date: " << getDateTime(allBanks[i].foundationDate) << std::endl;
+			std::cout << bright << cyan << "    ==========================================" << reset << std::endl << std::endl;
 		}
-		std::cout << "      User type: " << userType << std::endl;
-		std::cout << "      Full name: " << allUsers[i].name << std::endl;
-		std::cout << "      Phone number: " << allUsers[i].phoneNO << std::endl;
-		if (allUsers[i].isBlocked)
-			std::cout << bright << red << "      Is blocked: True" << reset << std::endl;
-		else
-			std::cout << bright << green << "      Is blocked: False" << reset << std::endl;
-
-		std::cout << bright << cyan << "    ==========================================" << reset << std::endl << std::endl;
+		std::string tmp;
+		std::getline(std::cin, tmp);
 	}
-	std::string tmp;
-	std::getline(std::cin, tmp);
-}
 
-void addBank()
-{
-	std::cout << std::endl << cyan << " Add Bank" << reset << std::endl << std::endl;
-
-	std::string bankName = getStringInput("Bank Name", true, true);
-	if (bankName == "-1") return;
-
-	int balance = getIntInput("Total Balance");
-	if (balance == -1) return;
-
-	newBank(bankName, balance);
-
-	std::cout << std::endl << green << bright << "Bank has been added successfully!" << reset << std::endl;
-	_sleep(1000);
-	system("cls");
-}
-
-void promoteUser()
-{
-	std::cout << std::endl << cyan << " Promote User" << reset << std::endl << std::endl;
-
-	std::string username = getStringInput("Username");
-	if (username == "-1") return;
-	while (!User::exists(username))
+	void usersList()
 	{
-		std::cout << red << "    Username doesn't exist!" << reset << std::endl;
-		username = getStringInput("Username");
+		system("cls");
+		std::cout << std::endl << cyan << "  Users :" << reset << std::endl << std::endl;
+		std::vector<User> allUsers = User::getAllUsers();
+		for (int i = 0; i < allUsers.size(); i++)
+		{
+			std::cout << "    " << std::to_string(i + 1) << "." << std::endl;
+			std::cout << bright << cyan << "    ==========================================" << reset << std::endl;
+
+			std::cout << "      Username: " << allUsers[i].username << std::endl;
+
+			std::string userType = "";
+			switch (allUsers[i].type)
+			{
+			case 1:
+				userType = "Normal user";
+				break;
+			case 2:
+				userType = "Bank manager";
+				break;
+			case 3:
+				userType = "Full admin";
+				break;
+			}
+			std::cout << "      User type: " << userType << std::endl;
+			std::cout << "      Full name: " << allUsers[i].name << std::endl;
+			std::cout << "      Phone number: " << allUsers[i].phoneNO << std::endl;
+			if (allUsers[i].isBlocked)
+				std::cout << bright << red << "      Is blocked: True" << reset << std::endl;
+			else
+				std::cout << bright << green << "      Is blocked: False" << reset << std::endl;
+
+			std::cout << bright << cyan << "    ==========================================" << reset << std::endl << std::endl;
+		}
+		std::string tmp;
+		std::getline(std::cin, tmp);
+	}
+
+	void addBank()
+	{
+		std::cout << std::endl << cyan << " Add Bank" << reset << std::endl << std::endl;
+
+		std::string bankName = getStringInput("Bank Name", true, true);
+		if (bankName == "-1") return;
+
+		int balance = getIntInput("Total Balance");
+		if (balance == -1) return;
+
+		newBank(bankName, balance);
+
+		std::cout << std::endl << green << bright << "Bank has been added successfully!" << reset << std::endl;
+		_sleep(1000);
+		system("cls");
+	}
+
+	void promoteUser()
+	{
+		std::cout << std::endl << cyan << " Promote User" << reset << std::endl << std::endl;
+
+		std::string username = getStringInput("Username");
 		if (username == "-1") return;
-	}
-
-	User manager = User::getUserStruct(username);
-	if (manager.type == 1)
-	{
-		int bankId = getIntInput("BankId");
-		if (bankId == -1) return;
-		while (!Bank::exists(bankId))
+		while (!User::exists(username))
 		{
-			std::cout << red << "    invalid BankId!" << reset << std::endl;
-			bankId = getIntInput("BankId");
-			if (bankId == -1) return;
+			std::cout << red << "    Username doesn't exist!" << reset << std::endl;
+			username = getStringInput("Username");
+			if (username == "-1") return;
 		}
 
-		manager.type = 2;
-		manager.bankId = bankId;
+		User manager = User::getUserStruct(username);
+		if (manager.type == 1)
+		{
+			int bankId = getIntInput("BankId");
+			if (bankId == -1) return;
+			while (!Bank::exists(bankId))
+			{
+				std::cout << red << "    invalid BankId!" << reset << std::endl;
+				bankId = getIntInput("BankId");
+				if (bankId == -1) return;
+			}
+
+			manager.type = 2;
+			manager.bankId = bankId;
+			User::addOrUpdateUser(manager);
+
+			Bank bank = Bank::getBankStruct(bankId);
+			bank.managers.push_back(manager.username);
+			Bank::addOrUpdateBank(bank);
+			std::cout << std::endl << green << bright << "Manager has been successfully added to bank!" << reset << std::endl;
+		}
+		else
+			std::cout << red << "    User is already a bank manager!" << reset << std::endl;
+
+		_sleep(1000);
+		system("cls");
+	}
+
+	void demoteUser()
+	{
+		std::cout << std::endl << cyan << " Demote User" << reset << std::endl << std::endl;
+
+		std::string username = getStringInput("Username");
+		if (username == "-1") return;
+		while (!User::exists(username))
+		{
+			std::cout << red << "    Username doesn't exist!" << reset << std::endl;
+			username = getStringInput("Username");
+			if (username == "-1") return;
+		}
+
+
+		User manager = User::getUserStruct(username);
+		manager.type = 1;
+
+		Bank bank = Bank::getBankStruct(manager.bankId);
+		bank.managers.erase(std::remove(bank.managers.begin(), bank.managers.end(), manager.username), bank.managers.end());
+		Bank::addOrUpdateBank(bank);
+
+		manager.bankId = 0;
 		User::addOrUpdateUser(manager);
 
-		Bank bank = Bank::getBankStruct(bankId);
-		bank.managers.push_back(manager.username);
-		Bank::addOrUpdateBank(bank);
-		std::cout << std::endl << green << bright << "Manager has been successfully added to bank!" << reset << std::endl;
+		std::cout << std::endl << green << bright << "Manager has been successfully removed from bank!" << reset << std::endl;
+		_sleep(1000);
+		system("cls");
 	}
-	else
-		std::cout << red << "    User is already a bank manager!" << reset << std::endl;
-
-	_sleep(1000);
-	system("cls");
-}
-
-void demoteUser()
-{
-	std::cout << std::endl << cyan << " Demote User" << reset << std::endl << std::endl;
-
-	std::string username = getStringInput("Username");
-	if (username == "-1") return;
-	while (!User::exists(username))
-	{
-		std::cout << red << "    Username doesn't exist!" << reset << std::endl;
-		username = getStringInput("Username");
-		if (username == "-1") return;
-	}
-
-
-	User manager = User::getUserStruct(username);
-	manager.type = 1;
-
-	Bank bank = Bank::getBankStruct(manager.bankId);
-	bank.managers.erase(std::remove(bank.managers.begin(), bank.managers.end(), manager.username), bank.managers.end());
-	Bank::addOrUpdateBank(bank);
-
-	manager.bankId = 0;
-	User::addOrUpdateUser(manager);
-
-	std::cout << std::endl << green << bright << "Manager has been successfully removed from bank!" << reset << std::endl;
-	_sleep(1000);
-	system("cls");
 }
